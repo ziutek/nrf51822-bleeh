b66bebcd69f503e7f8fc52cd6d4cca6032087319
Luca.Buccolini@gmail.com <Luca.Buccolini@gmail.com@49a9d224-c8a9-27d7-1ebb-64ad8577af68>
2014-10-06 21:59:37 +0000

    Delete nrf_delay_l85 and other custom delay files Development of LPCOMP driver. More informations on: https://code.google.com/p/nrf51822-bleeh/wiki/2b_Development_of_Comparator_Driver

--- trunk/bleeh/main.c
+++ trunk/bleeh/main.c
@@ -1,44 +1,66 @@
-/* ---------------------------------------------------------------------------
-** Filename: main.c
-**
-** Description: main file for Nordic Semiconductor nRF51822 Evaluation Kit 
-** (a.k.a. PCA10001)blinky example. This example file is derived from 
-** Nordic Semiconductor's "blinky_example" using a custom delay module.
-** This file contains the source code for a sample application using GPIO to
-** drive LEDs.
-**
-** Author: Luca Buccolini
-**
-** -------------------------------------------------------------------------*/
+/*****************************************************************************************************
+** -------------------------------------------------------------------------------------------------**
+** Filename: main.c																																									**												
+** -------------------------------------------------------------------------------------------------**	
+** Description: main file for Nordic Semiconductor nRF51822 Evaluation Kit 													**	
+** 							(a.k.a. PCA10001) 2b_Development_of_Comparator_Driver project: 											**
+**							development of comparator's driver using an external button 												**
+**							to trigger the comparator during development and experimental												**		
+**							phases. 																																						**
+**																																																	**
+** 							Full sedcription of this task is described here: 																		**		
+**							https://code.google.com/p/nrf51822-bleeh/wiki/2b_Development_of_Comparator_Driver		**
+**																																																	**
+**							This example shows the functionality of the low power comparator (LPCOMP)						**
+**							To test, connect a voltage source (or a button to Vcc) to pin P0.03 (AIN4). 				**
+**							When the voltage goes above Vcc/2=1.5 V, (LED0) is set.															**
+** 							When the voltage goes below Vcc/2=1.5 V, (LED0) is cleared.													**
+**							When the voltage on AIN6 either goes above or below the specified reference 				**
+**							voltage, (LED1) is toggled to show that the LPCOMP interrupt handler is executed.		**																																								**
+**																																																	**
+** -------------------------------------------------------------------------------------------------**
+** Author: Luca Buccolini																																						**
+** -------------------------------------------------------------------------------------------------**
+** Start date: Sep 3, 2014 																																					**
+** -------------------------------------------------------------------------------------------------**
+** Finish date:	work in progress																																		**
+** -------------------------------------------------------------------------------------------------**
+*****************************************************************************************************/
 
+#include <pca10001.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <nRF51.h>
-#include "nrf_gpio.h"
 #include <pca10001.h>
-#include "nrf_delay_l85.h"
 
-/**
+#include "nrf_gpio.h"
+#include "nrf51_LPCOMP.h"
+
+/*****************************************************************************************************
  * Main function
- */
+ *
+ * @param  none
+ * @return none
+ *
+ * @brief  Configures and enables the LPCOMP
+ *
+ ****************************************************************************************************/
+
 int main(void)
 {
-    // Configure LED-pins as outputs
-    nrf_gpio_cfg_output(LED_0);
-    nrf_gpio_cfg_output(LED_1);
+	// Configure LED-pins as outputs
+	nrf_gpio_cfg_output(LED_0);
+	nrf_gpio_cfg_output(LED_1);
+
+	//Initialize LPCOMP
+	LPCOMP_init();
+		
   
-    // LED 0 and LED 1 blink alternately.
-    while (true)
-    {
-        nrf_gpio_pin_clear(LED_0);
-        nrf_gpio_pin_set(LED_1);
-    
-			nrf_delay(100); // notice they're NOT ms!!
-    
-        nrf_gpio_pin_clear(LED_1);
-        nrf_gpio_pin_set(LED_0);
-    
-        nrf_delay(100); // notice they're NOT ms!!
-    }
+	// LED 0 and LED 1 blink alternately.
+	while (true)
+	{
+		//Put CPU to sleep while waiting for interrupt to save power
+		__WFI();
+	}
 }
 
