2b37c8434a20c8b94e7d4881406ee3b9c88501c9
Luca.Buccolini <Luca.Buccolini@gmail.com>
2014-12-06 14:21:01 +0000

    Step 6.1 accomplished & step 6.2 accomplished!! Ner revision of step 6.2. Clear old 6.2 in svn/tags/!

--- nrf51-ble-app-lbs-mod/main.c
+++ nrf51-ble-app-lbs-mod/main.c
@@ -30,7 +30,7 @@
 #include "app_button.h"
 #include "ble_debug_assert_handler.h"
 #include "pstorage.h"
-#include "ble_lbs.h"
+#include "ble_sss.h"
 #define IS_SRVC_CHANGED_CHARACT_PRESENT 0                                           /**< Include or not the service_changed characteristic. if not enabled, the server's database cannot be changed for the lifetime of the device*/
 
 #define WAKEUP_BUTTON_PIN               BUTTON_0                                    /**< Button used to wake up the application. */
