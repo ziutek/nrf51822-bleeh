26b8ae0fbcb0e07e733ed4240d71f5cbae312f27
Luca.Buccolini@gmail.com <Luca.Buccolini@gmail.com@49a9d224-c8a9-27d7-1ebb-64ad8577af68>
2015-02-18 14:23:37 +0000

    update connection parameters with (Conn.interval = 500 ms, Slave Latency = 3, Supervisory Timeout = 4000 ms)

--- trunk/nrf51-ble-app-lbs-mod/main.c
+++ trunk/nrf51-ble-app-lbs-mod/main.c
@@ -54,8 +54,8 @@
 
 #define MIN_CONN_INTERVAL               MSEC_TO_UNITS(500, UNIT_1_25_MS)            /**< Minimum acceptable connection interval (0.5 seconds). */
 #define MAX_CONN_INTERVAL               MSEC_TO_UNITS(500, UNIT_1_25_MS)            /**< Maximum acceptable connection interval (0.5 second). */
-#define SLAVE_LATENCY                   7                                           /**< Slave latency. */
-#define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(10000, UNIT_10_MS)             /**< Connection supervisory timeout (10 seconds). */
+#define SLAVE_LATENCY                   3                                           /**< Slave latency. */
+#define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)            /**< Connection supervisory timeout (4 seconds). */
 #define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(20000, APP_TIMER_PRESCALER) /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (15 seconds). */
 #define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(5000, APP_TIMER_PRESCALER)  /**< Time between each call to sd_ble_gap_conn_param_update after the first call (5 seconds). */
 #define MAX_CONN_PARAMS_UPDATE_COUNT    3                                           /**< Number of attempts before giving up the connection parameter negotiation. */
